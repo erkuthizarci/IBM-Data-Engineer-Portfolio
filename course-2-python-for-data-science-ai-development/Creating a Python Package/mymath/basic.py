ad8122682a8e21cce5dfed5367e914522380ce92
Erkut Hızarcı <155409141+erkuthizarci@users.noreply.github.com>
2026-01-22 15:01:07 +0100

    Deploy mymath package
    This commit finalizes the mymath package structure by converting Jupyter Notebooks into importable Python modules (.py). It includes basic and stats modules for mathematical operations, along with a standard __init__.py file to ensure the directory is recognized as a valid Python package.

---
+++ course-2-python-for-data-science-ai-development/Creating a Python Package/mymath/basic.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Objectives
+
+# - Create a module named basic  
+# - Add two functions to the module basic
+# - Create a module named stats
+# - Add two functions to the module stats
+# - Create a python package named mymath
+# - Verify that the package is working
+
+# In[ ]:
+
+
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a, b):
+    """
+    This function returns the sum of given numbers
+    """
+    return a + b
+
